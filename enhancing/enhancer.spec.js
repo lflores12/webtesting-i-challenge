a849dbf9161385b5a35e99d75ec331dc729a70f1
Leonel <leonelaf12@gmail.com>
2019-06-17 15:50:54 -0400

    made repair test and is passing

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,9 @@
-const enhancer = require('./enhancer.js');
+const { repair } = require('./enhancer.js');
 // test away!
+describe('enhancer.js', () => {
+    describe('repair()', () => {
+        it('restores durability to 100', () => {
+            expect(repair({ durability:89 }).durability).toBe(100);
+        })
+    })
+})
